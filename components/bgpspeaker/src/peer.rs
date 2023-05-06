04b90016d61d7513b0420cd255d9e56c32d7a0d7
monandkey <satoru070505@gmail.com>
2023-05-07 06:44:00 +0900

    Add tcp connection processing

--- components/bgpspeaker/src/peer.rs
+++ components/bgpspeaker/src/peer.rs
@@ -1,18 +1,16 @@
 use tracing::{debug, info, instrument};
 
 use crate::config::Config;
+use crate::connection::Connection;
 use crate::event::Event;
 use crate::event_queue::EventQueue;
 use crate::state::State;
 
-/// BGPのRFCで示されている実装方針
-/// (https://datatracker.ietf.org/doc/html/rfc4271#section-8)では、
-/// 1つのPeerを1つのイベント駆動ステートマシンとして実装しています。
-/// Peer構造体はRFC内で示されている実装方針に従ったイベント駆動ステートマシンです。
 #[derive(Debug)]
 pub struct Peer {
     state: State,
     event_queue: EventQueue,
+    tcp_connection: Option<Connection>,
     config: Config,
 }
 
@@ -23,6 +21,7 @@ impl Peer {
         Self {
             state,
             event_queue,
+            tcp_connection: None,
             config,
         }
     }
@@ -45,6 +44,12 @@ impl Peer {
         match &self.state {
             State::Idle => match event {
                 Event::ManualStart => {
+                    self.tcp_connection = Connection::connect(&self.config).await.ok();
+                    if self.tcp_connection.is_some() {
+                        self.event_queue.enqueue(Event::TcpConnectionConfirmed);
+                    } else {
+                        panic!("TCP Connectionの確立が出来ませんでした。{:?}", self.config)
+                    }
                     self.state = State::Connect;
                 }
                 _ => {}
@@ -57,11 +62,20 @@ impl Peer {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use tokio::time::{sleep, Duration};
+
     #[tokio::test]
     async fn peer_can_transition_to_connect_state() {
         let config: Config = "64512 127.0.0.1 65413 127.0.0.2 active".parse().unwrap();
         let mut peer = Peer::new(config);
         peer.start();
+        tokio::spawn(async move {
+            let remote_config = "64513 127.0.0.2 65412 127.0.0.1 passive".parse().unwrap();
+            let mut remote_peer = Peer::new(remote_config);
+            remote_peer.start();
+            remote_peer.next().await;
+        });
+        tokio::time::sleep(Duration::from_secs(1)).await;
         peer.next().await;
         assert_eq!(peer.state, State::Connect);
     }
